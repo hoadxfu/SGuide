b6a388f56d22827028014a054bf045381a353d3c
hoadx <xuanhoa.master@gmail.com>
2017-07-18 16:50:08 +0700

    add realm

--- android/app/src/main/java/com/sguide/MainApplication.java
+++ android/app/src/main/java/com/sguide/MainApplication.java
@@ -3,6 +3,7 @@ package com.sguide;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.realm.react.RealmReactPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RealmReactPackage(),
             new LinearGradientPackage(),
             new MapsPackage(),
             new RNDeviceInfo(),
